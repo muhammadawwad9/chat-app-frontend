97a836aaa6441f2c8db8b8ef5e0066ed5da4e84d
Muhammad Awwad <hammode.awad@gmail.com>
2021-04-03 00:05:14 +0300

    fixes

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,21 +2,16 @@ import React from "react";
 import "./Message.scss";
 import ReactEmoji from "react-emoji";
 const Message = ({ message, userName }) => {
-  if (message.userName != userName) {
-    return (
-      <div className="member-message">
-        <div className="userName">{message.userName}</div>
-        <div className="text">{ReactEmoji.emojify(message.text)}</div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="my-message">
-        <div className="userName">{message.userName}</div>
-        <div className="text">{ReactEmoji.emojify(message.text)}</div>
-      </div>
-    );
-  }
+  return (
+    <div
+      className={`Message ${
+        message.userName != userName ? "justify-start" : ""
+      }`}
+    >
+      <div className="userName">{message.userName}</div>
+      <div className="text">{ReactEmoji.emojify(message.text)}</div>
+    </div>
+  );
 };
 
 export default Message;

dfa9c8c6c33229c20d42f08a8c61f1aa777cb393
Muhammad Awwad <hammode.awad@gmail.com>
2021-04-03 00:27:45 +0300

    fixes

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -3,11 +3,7 @@ import "./Message.scss";
 import ReactEmoji from "react-emoji";
 const Message = ({ message, userName }) => {
   return (
-    <div
-      className={`Message ${
-        message.userName != userName ? "justify-start" : ""
-      }`}
-    >
+    <div className={userName == message.userName ? "Message" : "justify-start"}>
       <div className="userName">{message.userName}</div>
       <div className="text">{ReactEmoji.emojify(message.text)}</div>
     </div>

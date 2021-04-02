af22e71200d9fcdd627099cc325f1c7867ab2069
Muhammad Awwad <hammode.awad@gmail.com>
2021-04-02 23:41:44 +0300

    fxies

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import "./Message.scss";
 import ReactEmoji from "react-emoji";
 const Message = ({ message, userName }) => {
-  return (
-    <div
-      className={`Message ${
-        message.userName != userName ? "justify-start" : ""
-      }`}
-    >
-      <div className="userName">{message.userName}</div>
-      <div className="text">{ReactEmoji.emojify(message.text)}</div>
-    </div>
-  );
+  if (message.userName != userName) {
+    return (
+      <div className="member-message">
+        <div className="userName">{message.userName}</div>
+        <div className="text">{ReactEmoji.emojify(message.text)}</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="my-message">
+        <div className="userName">{message.userName}</div>
+        <div className="text">{ReactEmoji.emojify(message.text)}</div>
+      </div>
+    );
+  }
 };
 
 export default Message;

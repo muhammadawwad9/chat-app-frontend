0080546b5ea305764869ddb87122ddd5dbf18b5e
Muhammad Awwad <hammode.awad@gmail.com>
2021-04-11 02:11:47 +0300

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
 import App from "./app";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+reportWebVitals();
